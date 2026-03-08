88df3cef722e0a82a61c3153ecd56d2a223d2150
Michael Harms <harmsm@protonmail.com>
2026-03-08 10:44:55 -0700

    fixed generax compile

--- src/GeneRax/GeneRaxInstance.hpp
+++ src/GeneRax/GeneRaxInstance.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-
 #include "GeneRaxArguments.hpp"
-#include <string>
-#include <maths/Parameters.hpp>
-#include <maths/ModelParameters.hpp>
 #include <IO/FamiliesFileParser.hpp>
-
+#include <maths/ModelParameters.hpp>
+#include <maths/Parameters.hpp>
+#include <string>
 
 struct GeneRaxInstance {
   GeneRaxArguments args;
@@ -22,48 +20,33 @@ struct GeneRaxInstance {
   long elapsedSPR;
   long elapsedRaxml;
   unsigned int currentIteration;
-  
-  GeneRaxInstance(int argc, char** argv):
-    args(argc, argv),
-    totalLibpllLL(0),
-    totalRecLL(0),
-    elapsedRates(0),
-    elapsedSPR(0),
-    elapsedRaxml(0),
-    currentIteration(0)
-  {
-    auto recModel = ArgumentsHelper::strToRecModel(
-        args.reconciliationModelStr);
+
+  GeneRaxInstance(int argc, char **argv)
+      : args(argc, argv), totalLibpllLL(0), totalRecLL(0), elapsedRates(0),
+        elapsedSPR(0), elapsedRaxml(0), currentIteration(0) {
+    auto recModel = ArgumentsHelper::strToRecModel(args.reconciliationModelStr);
     std::string fractionMissingFile;
-    recModelInfo = RecModelInfo(recModel,
-        args.perFamilyDTLRates,
-        args.gammaCategories,
-        args.originationStrategy,
-        args.pruneSpeciesTree,
-        args.rootedGeneTree,
-        args.forceGeneTreeRoot,
-        args.madRooting,
-        args.minGeneBranchLength,
-        args.transferConstraint,
-        args.noDup,
+    recModelInfo = RecModelInfo(
+        recModel, RecOpt::Gradient, args.perFamilyDTLRates,
+        args.gammaCategories, args.originationStrategy, args.pruneSpeciesTree,
+        args.rootedGeneTree, args.forceGeneTreeRoot, args.madRooting,
+        args.minGeneBranchLength, args.transferConstraint, args.noDup,
         false, // no TL
         args.fractionMissingFile,
         false); // memory savings
     rates = getUserParameters();
   }
-  
+
   Parameters getUserParameters() const {
     Parameters user(args.dupRate, args.lossRate, args.transferRate);
     return recModelInfo.getParametersFromUser(user);
   }
 
   GeneRaxInstance(const GeneRaxInstance &) = delete;
-  GeneRaxInstance & operator = (const GeneRaxInstance &) = delete;
+  GeneRaxInstance &operator=(const GeneRaxInstance &) = delete;
   GeneRaxInstance(GeneRaxInstance &&) = delete;
-  GeneRaxInstance & operator = (GeneRaxInstance &&) = delete;
-  
+  GeneRaxInstance &operator=(GeneRaxInstance &&) = delete;
+
   void readModelParameters(ModelParameters &modelParameters);
   RecModelInfo getRecModelInfo();
 };
-
-
